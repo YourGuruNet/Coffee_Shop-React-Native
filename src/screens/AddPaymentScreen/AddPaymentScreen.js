db6b98b14ed65a7eceef2f80b72ee13dc279faa6
arvis <info@yourgurunet.com>
2021-02-25 11:16:56 +0200

    Add payment screen styles update

--- src/screens/AddPaymentScreen/AddPaymentScreen.js
+++ src/screens/AddPaymentScreen/AddPaymentScreen.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-
+import {Title, Container} from 'native-base';
+import {baseStyles} from '../../assets/styles';
+import {SafeAreaView} from 'react-native';
 const AddPaymentScreen = () => {
   return (
-    <View style={styles.textView}>
-      <Text style={styles.headerText}>Add your payment option</Text>
-    </View>
+    <Container style={baseStyles.container}>
+      <SafeAreaView />
+      <Title style={baseStyles.heading1}>Add your payment</Title>
+    </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  textView: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  headerText: {
-    fontSize: 30,
-    paddingVertical: 10,
-    color: 'black',
-  },
-});
 
 export default AddPaymentScreen;
